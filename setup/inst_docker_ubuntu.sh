a8552aca0e0c1b0aff7394dcbab445fb1509d6a6
Hezi Ismah Moshe <61164748+heziim@users.noreply.github.com>
2021-06-17 11:18:57 +0300

    Update inst_docker_ubuntu.sh

--- setup/inst_docker_ubuntu.sh
+++ setup/inst_docker_ubuntu.sh
@@ -14,7 +14,7 @@ sudo apt-get update
 sudo apt-get install docker-ce -y
 sudo usermod -aG docker $USER
 sudo docker run hello-world
-sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
